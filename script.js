1e8eca2e822b8cd6fa3121ba9a6af71e9d9d3d9f
jhoncarter7 <90902071+jhoncarter7@users.noreply.github.com>
2022-08-13 02:56:04 +0530

    update

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 const learnBtn = document.querySelector('.learn-btn');
 const section1 = document.querySelector('#section-1');
 const headOptions = document.querySelector('.head-options');
+const head = document.querySelector('.head');
+const subhead = document.querySelector('.sub-head')
 const optionbtn = document.querySelector('.options-btn');
 const tabs = document.querySelectorAll('.tabs');
 const sameSwap = document.querySelectorAll('.same-swap');
@@ -19,7 +21,6 @@ learnBtn.addEventListener('click', function (e) {
 
 headOptions.addEventListener('click', function (el) {
 
-   console.log( el.target)
   if(el.target.classList.contains('head-links')){
     el.preventDefault();
     el.target.getAttribute('href').scrollIntoView({behavior: 'smooth'})
@@ -45,25 +46,189 @@ optionbtn.addEventListener('click', function (e) {
 })
 
 
-// navbar
+// navbar fadding
 const navStyling = function (e) {
-  
-  if (e.target.classList.contains('head-links')){
+
+  if (e.target.classList.contains('feature-link')){
     const click = e.target
-     const sibblings = click.closest('.head').querySelectorAll('.head-links');
-     const logo = click.closest('.head').querySelector('img')
-     
+     const sibblings = click.closest('.head-options').querySelectorAll('.feature-link');
+     const logo = click.closest('.head').querySelector('img');
+   
        sibblings.forEach(el => { 
-        if(el !== click) el.style.opacity = this; })
+        if(el !== click) el.style.opacity = this; 
+      
+      })
         logo.style.opacity = this;
-        console.log(logo)
+     
       }
 }
 
-// if we use bind then it creat a function in which given value represented by 'this'
-headOptions.addEventListener('mouseover', navStyling.bind(0.1));
 
-headOptions.addEventListener('mouseout', navStyling.bind(1));
+// if we use bind then it create a function in which given value represented by 'this'
+head.addEventListener('mouseover', navStyling.bind(0.3));
+head.addEventListener('mouseout', navStyling.bind(1));
+
+
+
+
+// making navbar fixed when scroll to certain sections
+
+const header = document.querySelector('.header')
+const headHeight = head.getBoundingClientRect().height;
+const stickyNav = function(entries){
+
+  const [entry] = entries
+ if (!entry.isIntersecting)
+  head.classList.add('sticky-nav');
+  else   head.classList.remove('sticky-nav');  
+}
+
+const observer = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headHeight}px`
+});
+observer.observe(header)
+
+
+// making all section move little when we scroll
+const allsection = document.querySelectorAll('section')
+const revealSection = function(entries, observer){
+  const [entry] = entries;
+  
+  if(!entry.isIntersecting) return;
+    entry.target.classList.remove('heading-visibility')
+    observer.unobserve(entry.target)
+  
+}
+
+
+const scrollsection = new IntersectionObserver( revealSection, {
+  root: null,
+   threshold: 0.1
+})
+ allsection.forEach(function(section) { 
+  scrollsection.observe(section)
+   section.classList.add('heading-visibility')
+ })
+
+
+
+//  lazy image loding
+
+const targetImg = document.querySelectorAll("img[data-src]");
+const loadImg = function(entries, observer){
+ const [entry] = entries
+
+ if(!entry.isIntersecting) return;
+
+ entry.target.src = entry.target.dataset.src;
+ entry.target.classList.remove('lazy-img')
+ observer.unobserve(entry.target)
+
+}
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0.13,
+  //  rootMargin: '270px'
+})
+
+targetImg.forEach(function(img) {
+ imgObserver.observe(img)
+})
+
+
+
+// sliding in section 3
+
+const targetslide = document.querySelectorAll('.slide')
+const buttonR  = document.querySelector('.butt-right')
+const buttonL  = document.querySelector('.butt-left')
+
+const dotcontainer = document.querySelector('.dots')
+
+// 
+const activeDot = function(slides){
+ document.querySelectorAll('.dot').forEach( dot => dot.classList.remove('active-dot'))
+
+document.querySelector(`.dot[data-slide= "${slides}"]`).classList.add('active-dot')
+}
+
+
+
+
+
+const creatDots = function(){
+  targetslide.forEach(function(_, i) {
+    dotcontainer.insertAdjacentHTML('beforeend',
+     `<button class = "dot" data-slide = "${i}"> </button>`)
+  })
+}
+creatDots();
+
+
+let curslide = 0;
+let maxSlide = targetslide.length;
+
+targetslide.forEach((s, i) => 
+(s.style.transform = `translateX(${100 * i}%`))
+
+const getSlide = function(curslide){
+  targetslide.forEach((s, i) => 
+  (s.style.transform = `translateX(${100 * (i - curslide)}%`));
+  
+  
+}
+const nextSlide = function () {
+  if(curslide === maxSlide - 1){
+    curslide = 0;
+  }
+  else {
+    curslide++;
+  }
+  getSlide(curslide)
+  activeDot(curslide)
+}
+const prevSlide = function(){
+  if(curslide === 0){
+console.log(ok)
+    curslide = maxSlide - 1
+  }
+  else{
+
+    curslide--;
+  }
+  getSlide(curslide)
+  activeDot(curslide)
+}
+
+buttonR.addEventListener('click', nextSlide)
+
+buttonL.addEventListener('click', prevSlide)
+
+
+document.addEventListener('keydown', function(e){
+  if(  e.key === 'ArrowRight' )  nextSlide()
+  e.key === 'ArrowLeft'  && prevSlide()
+  
+
+})
+
+activeDot(0);
+dotcontainer.addEventListener('click', function(e){
+  if(e.target.classList.contains('dot')){
+    const slides = e.target.dataset.slide
+  //   we coan also write like
+  // const {slide} =  e.target.dataset
+  getSlide(slides)
+  activeDot(slides) 
+  }
+ 
+})
+
+
+
 
 //  practice of random color generate by child btn are also change parent color by bubbling methode. 
 
